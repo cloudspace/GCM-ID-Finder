3486c7d27ca36f9ce192f3f894193daaf173eb01
Ken Kyger <r2doesinc@gmail.com>
2015-04-14 11:34:53 -0400

    update travis config

---
+++ scripts/upload.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+  echo "This is a pull request. No deployment will be done."
+  exit 0
+fi
+if [[ "$TRAVIS_BRANCH" != "beta_release" ]]; then
+  echo "Testing on a branch other than beta_release. No deployment will be done."
+  exit 0
+fi
+
+dir=$PWD
+parent="$(dirname "$dir")"
+
+$parent/gradlew build
